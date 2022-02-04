515e225fb518750756e069c81f6b2dcecf51f18d
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-02-04 13:17:28 +1100

    Add reset button. Can change amount of rows and columns.

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const container = document.getElementById('container');
 let boxes = [];
 
-function createDivs(amount)
-{
-    for(let i = 0; i < amount; i++)
-    {
-        const div = document.createElement('div');
-        div.classList.add('box');
-        container.appendChild(div);
-        boxes.push(div);
-    }
-}
-
 function colorIn()
 {
     this.classList.add('colored')
 }
 
-createDivs(16*16);
+function createDivs(columns, rows)
+{
+    for(let x = 0; x < columns*rows; x++)
+    {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.style.width = `${100/columns}%`
+        div.style.height = `${100/columns}%`
+        container.appendChild(div);
+        div.addEventListener('mouseover', colorIn);
+        boxes.push(div);
+    }
+}
 
-boxes.forEach(box => {
-    box.addEventListener('mouseover', colorIn);
-});
+createDivs(16, 16);
+
+function reset()
+{   
+    let newColumns = prompt("How many columns?");
+    let newRows = prompt("How many rows?");
+
+    if(newColumns > 100 || newRows > 100) 
+    {
+        alert("Columns and rows cannot be over 100!")
+        return;
+    }
+
+    boxes.forEach(box => box.remove());
+    for(box of boxes) boxes.pop();
+
+    createDivs(newColumns, newRows);
+}
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', reset);
+
